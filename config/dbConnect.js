f11249afc5ef1d61ac54c3a0313911ab9c0978b2
anas baqai <anashabib139@gmail.com>
2024-02-13 14:01:45 +0500

    mongo db check ec2

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 module.exports = async() => {
     mongoose.set('strictQuery', true);
-   await mongoose.connect('mongodb+srv://anasbaqai9:Habib_03_BaqaI@cluster0.ektbxie.mongodb.net/whats-gud'||process.env.MONGODB_URL)
+   await mongoose.connect(process.env.MONGODB_URL)
         .then(() => console.log('Connected to DB'))
         .catch((error) => console.log("db error: ", error));
 }
